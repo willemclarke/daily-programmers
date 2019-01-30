c53209de79a3506dc012c8d26f0a95a24b9eaa8d
willemclarke <46552510+willemclarke@users.noreply.github.com>
2019-01-30 17:32:25 +1000

    Update challenge361.js

--- challenge361.js
+++ challenge361.js
@@ -9,49 +9,49 @@ function scoring(character) {
 	} else {
 			return console.log('Scoring Error')
 	}
-}
+};
 
 // Separated Letters from A-E
 const separatedA = splitInput.filter((character) => {
 	return character === 'a' || character === 'A'
-})
+});
 const separatedB = splitInput.filter((character) => {
 	return character === 'b' || character === 'B'
-})
+});
 const separatedC = splitInput.filter((character) => {
 	return character === 'c' || character === 'C'
-})
+});
 const separatedD = splitInput.filter((character) => {
 	return character === 'd' || character === 'D'
-})
+});
 const separatedE = splitInput.filter((character) => {
 	return character === 'e' || character === 'E'
-})
+});
 
 // Following 3 Lines = Scoring for A:
-const scoreA = separatedA.map(scoring)
-const totalA = scoreA.reduce((a, b) => a + b, 0)
-const printedA = console.log('a:' + totalA)
+const scoreA = separatedA.map(scoring);
+const totalA = scoreA.reduce((a, b) => a + b, 0);
+const printedA = console.log('a:' + totalA);
 
 // Following 3 Lines = Scoring for B:
-const scoreB = separatedB.map(scoring)
-const totalB = scoreB.reduce((a, b) => a + b, 0)
-const printedB = console.log('b:' + totalB)
+const scoreB = separatedB.map(scoring);
+const totalB = scoreB.reduce((a, b) => a + b, 0);
+const printedB = console.log('b:' + totalB);
 
 // Following 3 Lines = Scoring for C:
-const scoreC = separatedC.map(scoring)
-const totalC = scoreC.reduce((a, b) => a + b, 0)
-const printedC = console.log('c:' + totalC)
+const scoreC = separatedC.map(scoring);
+const totalC = scoreC.reduce((a, b) => a + b, 0);
+const printedC = console.log('c:' + totalC);
 
 // Following 3 Lines = Scoring for D:
-const scoreD = separatedD.map(scoring)
-const totalD = scoreD.reduce((a, b) => a + b, 0)
-const printedD = console.log('d:' + totalD)
+const scoreD = separatedD.map(scoring);
+const totalD = scoreD.reduce((a, b) => a + b, 0);
+const printedD = console.log('d:' + totalD);
 
 // Following 3 Lines = Scoring for E:
-const scoreE = separatedE.map(scoring)
-const totalE = scoreE.reduce((a, b) => a + b, 0)
-const printedE = console.log('e:' + totalE)
+const scoreE = separatedE.map(scoring);
+const totalE = scoreE.reduce((a, b) => a + b, 0);
+const printedE = console.log('e:' + totalE);
 
 // Output:
 // a:1
