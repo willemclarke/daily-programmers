a302cd68a80db2c8dbecf7b1074b502538c3166b
willemclarke <46552510+willemclarke@users.noreply.github.com>
2019-01-30 17:30:37 +1000

    Update challenge208.js

--- challenge208.js
+++ challenge208.js
@@ -1,12 +1,13 @@
-const rawInput = "3 1 3 4 4 1 4 5 2 1 4 4 4 4 1 4 3 2 5 5 2 2 2 4 2 4 4 4 4 1"
-const input = rawInput.split(' ')
+const rawInput = "3 1 3 4 4 1 4 5 2 1 4 4 4 4 1 4 3 2 5 5 2 2 2 4 2 4 4 4 4 1";
+const input = rawInput.split(' ');
 
 function cull(arr){
   return arr.filter(function(integer, index) {
     return arr.indexOf(integer) >= index;
   })
-}
-console.log(cull(input))
+};
+
+console.log(cull(input));
 
 // Output
 // [ '3', '1', '4', '5', '2' ]
