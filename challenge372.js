5ee19d6810d496b8db652300591216284d40bf7a
willemclarke <46552510+willemclarke@users.noreply.github.com>
2019-01-31 14:39:02 +1000

    Add files via upload

---
+++ challenge372.js
@@ -0,0 +1,41 @@
+function perfectlyBalanced(str) {
+  if (str.split("x").length === str.split("y").length) {
+    return true;
+    } else {
+    return false;
+    }
+}
+const input = "xxxyyyzzz";
+const inputOne = "yyyxxx";
+const inputTwo = "xxxyyyy";
+const inputThree = "yyxyxxyxxyyyyxxxyxyx";
+const inputFour = "xxyxxxxyyyxyxxyxxyy";
+const inputFive = "";
+const inputSix = "x";
+
+console.log(perfectlyBalanced(input)); // outputs true
+console.log(perfectlyBalanced(inputOne)); // outputs true
+console.log(perfectlyBalanced(inputTwo)); // outputs false
+console.log(perfectlyBalanced(inputThree)); // outputs true
+console.log(perfectlyBalanced(inputFour)); // outputs false
+console.log(perfectlyBalanced(inputFive)); // outputs true
+console.log(perfectlyBalanced(inputSix)); // outputs false
+
+// Output:
+// true
+// true
+// false
+// true
+// false
+// true
+// false
+
+
+
+
+
+
+
+
+
+
